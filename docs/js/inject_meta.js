7f1c5c31e8a6787879c6645b06f6d0f4eebf9a40
miguelzeph <miguel.junior.mat@hotmail.com>
2024-08-10 16:52:41 +0100

    adding google console search

---
+++ docs/js/inject_meta.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var meta = document.createElement('meta');
+    meta.name = "google-site-verification";
+    meta.content = "3UoZUlCMwNP_4QnYjXc-dXQeiiCkzQBJABzp5EhCX1g";
+    document.head.appendChild(meta);
+});
