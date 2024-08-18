3bd6f872b5032590b3a3a289fad06a0e50763611
miguelzeph <miguel.junior.mat@hotmail.com>
2024-08-18 23:30:56 +0100

    Updating documentation

--- site/js/inject_meta.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var meta = document.createElement('meta');
-    meta.name = "google-site-verification";
-    meta.content = "3UoZUlCMwNP_4QnYjXc-dXQeiiCkzQBJABzp5EhCX1g";
-    document.head.appendChild(meta);
-});
